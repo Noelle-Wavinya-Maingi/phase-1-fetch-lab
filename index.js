794857fdce7ecd1125938e4d93360b0d4a551101
Noelle <noelle.maingi@student.moringaschool.com>
2023-06-23 14:39:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(bookdata => bookdata.forEach(books => {
+    renderBooks(bookdata)
+  }))
+  
   
 }
 
 function renderBooks(books) {
+  //fetchBooks();
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
@@ -15,3 +22,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
